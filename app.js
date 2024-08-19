1865ad98f9752728eb728c518f0b84500eef7608
Motiv <131569759+successSpirite@users.noreply.github.com>
2024-08-18 20:12:03 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 const body = document.querySelector(".body");
+const videoPlayer = document.querySelector(".video");
 
+// enable sound 
+videoPlayer.muted = false;
+
+// play the video automatically 
+videoPlayer.play(); 
 const colors = ["red", "blue", "purple", "yellowgreen", "orange"];
 let colorIndex  = 0;
 
