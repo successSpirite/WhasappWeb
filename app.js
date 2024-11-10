8530e3327c7899b8ac7d02cde4ba189064a4e76f
Motiv <131569759+successSpirite@users.noreply.github.com>
2024-11-09 22:57:07 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-const body = document.querySelector(".body");
-const videoPlayer = document.querySelector(".video");
+// const body = document.querySelector(".body");
+// const videoPlayer = document.querySelector(".video");
 
-// enable sound 
-videoPlayer.muted = false;
+// // enable sound 
+// videoPlayer.muted = false;
 
-// play the video automatically 
-videoPlayer.play(); 
-const colors = ["red", "blue", "purple", "yellowgreen", "orange"];
-let colorIndex  = 0;
+// // play the video automatically 
+// videoPlayer.play(); 
+// const colors = ["red", "blue", "purple", "yellowgreen", "orange"];
+// let colorIndex  = 0;
 
-setInterval(() => {
-  body.style.background = colors[colorIndex];
-  colorIndex = (colorIndex + 1)% colors.length;
-}, 5000);
+// setInterval(() => {
+//   body.style.background = colors[colorIndex];
+//   colorIndex = (colorIndex + 1)% colors.length;
+// }, 5000);
